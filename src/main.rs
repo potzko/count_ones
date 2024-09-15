7ad483ac759f3261710705d5e5b5682241ef0f36
Omer <orodnay@gmail.com>
2024-09-15 18:01:30 +0300

    added a more readable count ones inpace 5

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use core::num;
 use lazy_static::lazy_static;
 use rand::Rng;
 use std::{hint::black_box, time::Instant};
@@ -109,11 +110,27 @@ fn count_ones_inplace_4(num: NumType) -> u8 {
     //we calculate those at the same time using instruction over the whole number rather then just checking one bit at a time
     let masks = [0x55555555, 0x33333333, 0x0f0f0f0f, 0x00ff00ff, 0x0000ffff];
     let mut ret = num - ((num >> 1) & masks[0]);
-    ret = ((ret >> (1 << 1)) & masks[1]) + (ret & masks[1]);
-    ret = ((ret >> (1 << 2)) + ret) & masks[2];
-    ret = ((ret >> (1 << 3)) + ret) & masks[3];
-    ret = ((ret >> (1 << 4)) + ret) & masks[4];
+    ret = ((ret >> (2)) & masks[1]) + (ret & masks[1]);
+    ret = ((ret >> (4)) + ret) & masks[2];
+    ret = ((ret >> (8)) + ret) & masks[3];
+    ret = ((ret >> (16)) + ret) & masks[4];
     ret as u8
+}
+
+#[no_mangle]
+fn count_ones_inplace_5(num: NumType) -> u8 {
+    let mut num = num;
+    //note, numtype can only be up to u32
+    //set ones in num = set ones in left half of num + set ones in right half of num
+    //we calculate those at the same time using instruction over the whole number rather then just checking one bit at a time
+    let masks = [0x55555555, 0x33333333, 0x0f0f0f0f, 0x00ff00ff, 0x0000ffff];
+    let inverse_masks = [0xaaaaaaaa, 0xcccccccc, 0xf0f0f0f0, 0xff00ff00, 0xffff0000];
+    num = (num & masks[0]) + ((num & inverse_masks[0]) >> 1);
+    num = (num & masks[1]) + ((num & inverse_masks[1]) >> 2);
+    num = (num & masks[2]) + ((num & inverse_masks[2]) >> 4);
+    num = (num & masks[3]) + ((num & inverse_masks[3]) >> 8);
+    num = (num & masks[4]) + ((num & inverse_masks[4]) >> 16);
+    num as u8
 }
 
 fn count_ones_memo_0(num: NumType) -> u8 {
@@ -145,10 +162,9 @@ fn count_ones_memo_2(num: NumType) -> u8 {
     a + b + c + d
 }
 
-fn measure_execution_time<F, NumType>(nums: &[NumType], count_fn: F) -> std::time::Duration
+fn measure_execution_time<F>(nums: &[NumType], count_fn: F) -> std::time::Duration
 where
     F: Fn(NumType) -> u8,
-    NumType: Copy,
 {
     let start = std::time::Instant::now();
     for &i in nums {
@@ -157,6 +173,15 @@ where
     start.elapsed()
 }
 
+fn check_correct<F>(nums: &[NumType], count_fn: F) -> bool
+where
+    F: Fn(NumType) -> u8,
+{
+    nums.iter()
+        .map(|i| inbuilt_count_ones(*i))
+        .collect::<Vec<u8>>()
+        == nums.iter().map(|i| count_fn(*i)).collect::<Vec<u8>>()
+}
 fn run_vec(nums: &[NumType]) {
     let time_count_inplace_naive = measure_execution_time(nums, count_ones_inplace_naive);
     let time_count_inplace = measure_execution_time(nums, count_ones_inplace_0);
@@ -164,22 +189,68 @@ fn run_vec(nums: &[NumType]) {
     let time_count_inplace_2 = measure_execution_time(nums, count_ones_inplace_2);
     let time_count_inplace_3 = measure_execution_time(nums, count_ones_inplace_3);
     let time_count_inplace_4 = measure_execution_time(nums, count_ones_inplace_4);
+    let time_count_inplace_5 = measure_execution_time(nums, count_ones_inplace_5);
     let time_count_memo = measure_execution_time(nums, count_ones_memo_0);
     let time_count_memo_1 = measure_execution_time(nums, count_ones_memo_1);
     let time_count_memo_2 = measure_execution_time(nums, count_ones_memo_2);
     let time_inbuilt = measure_execution_time(nums, inbuilt_count_ones);
 
     println!("Count Methods Execution Times:");
-    println!("  Inplace Naive:          {:?}", time_count_inplace_naive);
-    println!("  Inplace 0:              {:?}", time_count_inplace);
-    println!("  Inplace 1:              {:?}", time_count_inplace_1);
-    println!("  Inplace 2:              {:?}", time_count_inplace_2);
-    println!("  Inplace 3:              {:?}", time_count_inplace_3);
-    println!("  Inplace 4:              {:?}", time_count_inplace_4);
-    println!("  Memoization:            {:?}", time_count_memo);
-    println!("  Memoization 1:          {:?}", time_count_memo_1);
-    println!("  Memoization 2:          {:?}", time_count_memo_2);
-    println!("  Inbuilt Function:       {:?}", time_inbuilt);
+    println!(
+        "  Inplace Naive:          {:?}, {}",
+        time_count_inplace_naive,
+        check_correct(nums, count_ones_inplace_naive)
+    );
+    println!(
+        "  Inplace 0:              {:?}, {}",
+        time_count_inplace,
+        check_correct(nums, count_ones_inplace_0)
+    );
+    println!(
+        "  Inplace 1:              {:?}, {}",
+        time_count_inplace_1,
+        check_correct(nums, count_ones_inplace_1)
+    );
+    println!(
+        "  Inplace 2:              {:?}, {}",
+        time_count_inplace_2,
+        check_correct(nums, count_ones_inplace_2)
+    );
+    println!(
+        "  Inplace 3:              {:?}, {}",
+        time_count_inplace_3,
+        check_correct(nums, count_ones_inplace_3)
+    );
+    println!(
+        "  Inplace 4:              {:?}, {}",
+        time_count_inplace_4,
+        check_correct(nums, count_ones_inplace_4)
+    );
+    println!(
+        "  Inplace 5:              {:?}, {}",
+        time_count_inplace_5,
+        check_correct(nums, count_ones_inplace_5)
+    );
+    println!(
+        "  Memoization:            {:?}, {}",
+        time_count_memo,
+        check_correct(nums, count_ones_memo_0)
+    );
+    println!(
+        "  Memoization 1:          {:?}, {}",
+        time_count_memo_1,
+        check_correct(nums, count_ones_memo_1)
+    );
+    println!(
+        "  Memoization 2:          {:?}, {}",
+        time_count_memo_2,
+        check_correct(nums, count_ones_memo_2)
+    );
+    println!(
+        "  Inbuilt Function:       {:?}, {}",
+        time_inbuilt,
+        check_correct(nums, inbuilt_count_ones)
+    );
 }
 
 lazy_static! {
